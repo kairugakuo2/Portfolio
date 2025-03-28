9670dcd711d50a834e43459dc84bf5109b7166ac
Gakuo <kairugakuo9@gmail.com>
2025-03-28 16:56:52 -0500

    fixed mobile fomatting issue with  text wrapping

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="navContainer">
-                <div id="leftNav">
-                    <a href="#" className="navbar-brand">Gakuo Kairu</a>
+                {/* Brand / Name */}
+                <a href="#" className="navbar-brand">Gakuo Kairu</a>
 
-                    <a href='#about'>About</a>
-                    <a href='#experience'>Experience</a>
-                    <a href='#projects'>Projects</a>
-                </div>
+                {/* Hamburger icon (only shows on mobile) */}
+                <button
+                    className="hamburger"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    aria-label="Toggle menu"
+                >
+                    ☰
+                </button>
 
-                <div id="rightNav">
-                    <a target="_blank" href="mailto:kairugakuo2@gmail.com">Email</a>
-                    <a target="_blank" href="https://github.com/kairugakuo2">GitHub</a>
-                    <a target="_blank" href="https://www.linkedin.com/in/gakuo/">Linkedin</a>
+                {/* Full nav links – always visible on desktop, dropdown on mobile */}
+                <div className={`navLinks ${menuOpen ? "open" : ""}`}>
+                    <div id="leftNav">
+                        <a href="#about">About</a>
+                        <a href="#experience">Experience</a>
+                        <a href="#projects">Projects</a>
+                    </div>
+                    <div id="rightNav">
+                        <a target="_blank" href="mailto:kairugakuo2@gmail.com">Email</a>
+                        <a target="_blank" href="https://github.com/kairugakuo2">GitHub</a>
+                        <a target="_blank" href="https://www.linkedin.com/in/gakuo/">LinkedIn</a>
+                    </div>
                 </div>
             </div>
         </nav>
     );
 };
 
-export default NavBar
+export default NavBar;
